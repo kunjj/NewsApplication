c7a2932bd0a5e62d60f7c6fc7a79175f9522c65f
kunjjann <127756197+kunjjann@users.noreply.github.com>
2023-12-23 17:07:42 +0530

    Added Jvm To Project

--- app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.newsapplication
+package com.example.newsapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsapplication.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
