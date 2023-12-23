66cb82e9b7fa3a476d980f9b3f4f3ca5e3c91b47
kunjjann <127756197+kunjjann@users.noreply.github.com>
2023-12-23 17:31:05 +0530

    Enabled Data binding.

--- app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
@@ -1,12 +1,16 @@
 package com.example.newsapplication.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.newsapplication.R
+import com.example.newsapplication.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_news)
+
     }
 }
