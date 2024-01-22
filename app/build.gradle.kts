dff71c6012abb0a5dd03b00ec348e04451aa4869
Jain Kunjan <kunjan.j@wellnest.tech>
2024-01-22 17:31:42 +0530

    pxmps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.5.0")
 
-
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
