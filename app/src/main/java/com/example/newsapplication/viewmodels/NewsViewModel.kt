b3d921be3146d55d40347bf5b93f1d30e7cef457
Jain Kunjan <kunjan.j@wellnest.tech>
2024-01-25 15:11:22 +0530

    Completed Response Handling.

--- app/src/main/java/com/example/newsapplication/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapplication/viewmodels/NewsViewModel.kt
@@ -1,7 +1,29 @@
 package com.example.newsapplication.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapplication.models.News
 import com.example.newsapplication.repository.NewsRepository
+import com.example.newsapplication.utils.Result
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class NewsViewModel(val newsRepository : NewsRepository) : ViewModel() {
+class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
+    val news: MutableLiveData<Result<News>> = MutableLiveData()
+    val errorMsg = MutableLiveData<String>()
+    private var pageNumber = 1
+
+    fun getNews(countryCode: String) =
+        viewModelScope.launch {
+            news.postValue(Result.Loading())
+            val response = newsRepository.getNews(countryCode, pageNumber)
+            news.postValue(handleNewsResponse(response))
+        }
+
+    private fun handleNewsResponse(response: Response<News>): Result<News> {
+        if (response.isSuccessful) {
+            response.body()!!.let { newsResponse -> return Result.Success(newsResponse) }
+        } else return Result.Error(response.message())
+    }
 }
