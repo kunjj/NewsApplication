fcfd56e45d4b7739f413b44f197ec326e808c789
Jain Kunjan <kunjan.j@wellnest.tech>
2024-01-25 15:48:27 +0530

    cmskmds

--- app/src/main/java/com/example/newsapplication/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsapplication/ui/fragments/BreakingNewsFragment.kt
@@ -1,20 +1,22 @@
 package com.example.newsapplication.ui.fragments
 
 import android.os.Bundle
-import android.provider.ContactsContract.Data
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapplication.R
+import com.example.newsapplication.adapter.NewsAdapter
 import com.example.newsapplication.databinding.FragmentBreakingNewsBinding
 import com.example.newsapplication.ui.NewsActivity
 import com.example.newsapplication.viewmodels.NewsViewModel
 
 class BreakingNewsFragment : Fragment() {
-    private lateinit var binding : FragmentBreakingNewsBinding
-    private val viewmodel : NewsViewModel by lazy { (activity as NewsActivity).newsViewModel }
+    private lateinit var binding: FragmentBreakingNewsBinding
+    private val viewModel: NewsViewModel by lazy { (activity as NewsActivity).newsViewModel }
+    private lateinit var newsAdapter: NewsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +28,17 @@ class BreakingNewsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_breaking_news,container,false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_breaking_news, container, false)
+        setupRecyclerView()
         return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        this.newsAdapter = NewsAdapter()
+        binding.rvNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(requireActivity())
+        }
     }
 }
